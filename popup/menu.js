bc5f45b7f5c7a0a791ba638e44b87b50a80fea35
DanaVarahi <dana_varahi@protonmail.com>
2017-11-16 19:14:19 +0000

    Added ChainBB tag links
    Issue #2
    
    Added support for switching between ChainBB and other sites tag
    listings.

--- popup/menu.js
+++ popup/menu.js
@@ -1,57 +1,65 @@
 function onCreated(tab) {
-  console.error(`Created new tab: ${tab.id}`);
-  window.close();
+	console.error(`Created new tab: ${tab.id}`);
+	window.close();
 }
 
 function onError(error) {
-  console.error(`Error: ${error}`);
-  window.close();
+	console.error(`Error: ${error}`);
+	window.close();
 }
 
 document.addEventListener("click", (e) => {
-  if (e.target.classList.contains("steemswitch")) {
-        var destinationName = e.target.textContent;
-        var destination;
+	if (e.target.classList.contains("steemswitch")) {
+		var destinationName = e.target.textContent;
+		var destination;
 
-			  switch(destinationName) {
-			    case "Steemit":
-			      destination = "https://steemit.com";
-			      break;
-			    case "Busy":
-			      destination = "https://busy.org";
-			      break;
-			    case "Chainbb":
-			      destination = "https://beta.chainbb.com";
-			      break;
-          case "Utopian":
-  			      destination = "https://utopian.io";
-  			      break;
-          case "Yehey":
-      			  destination = "https://yehey.org";
-      			  break;
-			    case "Steemd":
-			      destination = "https://steemd.com";
-			      break;
-			    case "Steemdb":
-			      destination = "https://steemdb.com";
-			      break;
-			  }
+		switch(destinationName) {
+			case "Steemit":
+				destination = "https://steemit.com";
+				break;
+			case "Busy":
+				destination = "https://busy.org";
+				break;
+			case "Chainbb":
+				destination = "https://beta.chainbb.com";
+				break;
+			case "Utopian":
+				destination = "https://utopian.io";
+				break;
+			case "Yehey":
+				destination = "https://yehey.org";
+				break;
+			case "Steemd":
+				destination = "https://steemd.com";
+				break;
+			case "Steemdb":
+				destination = "https://steemdb.com";
+				break;
+		}
 
-        var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
-        gettingActiveTab.then((tabs) => {
-					  var thisUrl = tabs[0].url;
+		var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
+		gettingActiveTab.then((tabs) => {
+			var thisUrl = tabs[0].url;
 
-					  // This pattern will be used to get a hostname from the url
-					  var pattern=/(.+:\/\/)?([^\/]+)(\/.*)*/i;
-					  var arr=pattern.exec(thisUrl);
-					  var thisSite = arr[2];
+			// This pattern will be used to get a hostname from the url
+			var pattern=/(.+:\/\/)?([^\/]+)(\/.*)*/i;
+			var arr=pattern.exec(thisUrl);
+			var thisSite = arr[2];
 
-					  var newUrl = thisUrl.replace('https://'+thisSite,destination);
+			var newUrl = thisUrl.replace('https://'+thisSite,destination);
+			if (destinationName=="Chainbb"){
+				newUrl = newUrl.replace('/trending/','/topic/');
+				newUrl = newUrl.replace('/created/','/topic/');
+				newUrl = newUrl.replace('/hot/','/topic/');
+				newUrl = newUrl.replace('/promoted/','/topic/');
+			} else {
+				newUrl = newUrl.replace('/topic/','/created/');
+			}
 
-						var creating = browser.tabs.create({
-							url:newUrl
-						});
-						creating.then(onCreated, onError);
-        });
-  }
+			var creating = browser.tabs.create({
+				url:newUrl
+			});
+			creating.then(onCreated, onError);
+		});
+	}
 });
